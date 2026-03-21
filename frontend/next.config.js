9234adc092ea830c9004e488af57cb2dc846de56
Rajamahindra <rajam@digitaldiaries.com>
2026-03-22 00:18:36 +0530

    fix: remove deprecated images.domains from next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,14 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: [
-      "res.cloudinary.com",
-      "avatars.githubusercontent.com",
-      "lh3.googleusercontent.com",
-      "i.ibb.co",
-    ],
     remotePatterns: [
       { protocol: "https", hostname: "**" },
+      { protocol: "http", hostname: "localhost" },
     ],
   },
 };
